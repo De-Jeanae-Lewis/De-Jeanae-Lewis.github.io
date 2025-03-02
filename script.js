5ac8f190b8ce268e831f9f5050d9efb28e98433e
De-Jeanae-Lewis <de-jeanae.lewis@mohawkcollege.ca>
2025-03-02 14:09:02 -0500

    Added cursor and made changes to loading screen

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Close load
 window.addEventListener('load', function() {
     setTimeout(function() {
-        document.getElementById('loader-container').style.display = 'none';
-        window.location.href = 'index.html';
-    }, 3000);
+        document.getElementById('loader-container').style.opacity = '0';
+        
+        setTimeout(() => {
+            document.getElementById('loader-container').style.display = 'none';
+            document.getElementById('main-content').classList.remove('hidden');
+        }, 500);
+
+    }, 2000); 
 });
+
